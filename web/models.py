d339e74a07dc7547b5288eb8de20a7900799219d
hamidnet220 <kiani.hamidreza@gmail.com>
2019-03-29 13:44:24 +0430

    add Send model to chapar models and migrate it to database

--- web/models.py
+++ web/models.py
@@ -17,3 +17,13 @@ class Recive(models.Model):
     def __str__(self):
         return "{}-{}".format(self.title,self.recive_date)
     
+class Send(models.Model):
+    add_by_usr   = models.ForeignKey(User,on_delete=models.CASCADE)
+    title        = models.CharField(max_length = 150,validators=[empty_validate_event])
+    summery      = models.TextField(blank=True)
+    send_date  = models.DateTimeField()
+    send_number= models.CharField(max_length = 150)
+    description  = models.TextField(blank=True,null=True)
+
+    def __str__(self):
+        return "{}-{}".format(self.title,self.send_date)
