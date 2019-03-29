2a1e534802d0ef426fb6cb67a4aed2fc03b4f517
hamidnet220 <kiani.hamidreza@gmail.com>
2019-03-29 13:45:37 +0430

    register send model to admin site

--- web/admin.py
+++ web/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Recive
+from .models import Recive,Send
 
 # Register your models here.
 
 admin.site.register(Recive)
+admin.site.register(Send)
